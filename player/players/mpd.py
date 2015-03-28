5dae56af41104dfb77b462697507d6c0f485e432
themylogin <themylogin@gmail.com>
2015-03-28 17:51:12 +0600

    become_superseeded

--- player/players/mpd.py
+++ player/players/mpd.py
@@ -9,13 +9,18 @@ class MPD(object):
         self.client = mpd.MPDClient()
         self.client.connect(hostname, port)
 
-    def current_state(self):
+    def become_superseeded(self):
         playlistinfo = self.client.playlistinfo()
         status = self.client.status()
+        self.client.clear()
         return {
             "playlist"  : [item["file"] for item in playlistinfo],
             "position"  : int(status.get("song", -1)),
             "elapsed"   : int(float(status.get("elapsed", -1))),
+            "scrobbled" : (int(float(status.get("elapsed", -1))) >
+                               min(int(playlistinfo[int(status.get("song"))]["time"]) / 2, 240)
+                           if int(status.get("song", -1)) >= 0
+                           else False)
         }
 
     def update(self, path):
